0fc9e3dc6289bfe82ed803b9eabd46df66b34747
Jordan Linton <jordaneddielinton93@hotmail.co.uk>
2021-07-14 16:39:25 +0100

    remove sets and order from heighest to lowwest

--- main.js
+++ main.js
@@ -2,4 +2,19 @@
 👉 Write your kata here!
 */
 
+
+// test("given an array of numbers, remove all double's and reverse the list e:g")
+// [1,1,2,2,3,3,4,4]
+
 //👉 Write the function your CodeWarriors will start with below here:
+
+
+
+
+ function removeAndReverse(array){
+  let newArray = [...new Set(array)]
+  let returned = newArray.sort((a,b)=>b-a)
+  return returned
+}
+
+module.exports = removeAndReverse
