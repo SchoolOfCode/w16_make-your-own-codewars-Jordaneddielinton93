1bb817f5edadd919bbebb34bfcc33b6766bece19
Jordan Linton <jordaneddielinton93@hotmail.co.uk>
2021-07-14 16:45:08 +0100

    removed solutions

--- main.js
+++ main.js
@@ -4,17 +4,15 @@
 
 
 // test("given an array of numbers, remove all double's and reverse the list e:g")
-// [1,1,2,2,3,3,4,4]
 
 //👉 Write the function your CodeWarriors will start with below here:
-
+  
 
 
 
  function removeAndReverse(array){
-  let newArray = [...new Set(array)]
-  let returned = newArray.sort((a,b)=>b-a)
-  return returned
+  
+  return array
 }
 
 module.exports = removeAndReverse
